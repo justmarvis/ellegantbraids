1dca8dea35e584e409669a1e43ebfb502a9ff2bb
justmarvis <playdeadcanyon@gamil.com>
2022-06-20 20:25:11 -0600

    Beginning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { MenuComponent } from './menu/menu.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponent,
+    MenuComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule
